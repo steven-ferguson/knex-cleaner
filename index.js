426b1b0eb20f846afb9b2692b42e65ffd7639248
Steven Ferguson <steven.ferguson11@gmail.com>
2015-03-09 17:52:45 -0700

    Add method to clean tables with deletion

---
+++ index.js
@@ -0,0 +1,26 @@
+var Promise = require('bluebird');
+
+module.exports = {
+  clean: function(knexObject) {
+    var _this = this;
+
+    return knexObject.raw('show tables').then(function(tables) {
+      var tableNamesToClean = _this._getTablesToClean(tables[0]);
+
+      return Promise.map(tablesToClean, function(tableName) {
+        return cleanTableWithDeletion(knexObject, tableName);
+      });
+    });
+  },
+
+  cleanTableWithDeletion: function(knex, tableName) {
+    return knex.select().from(tableName).del();
+  },
+
+  _getTablesToClean: function(tables) {
+    return tables.filter(function(table) {
+      var tableName = table[Object.keys(table)[0]];
+      return tableName !== 'DATABASECHANGELOG' && tableName !== 'DATABASECHANGELOGLOCK';
+    });
+  }
+}
